abd6e418f912b77aacbd63c006bb3c8e13ec5c73
lukepoppe <lukepoppe@hotmail.com>
2015-04-17 15:52:34 -0500

    v1

--- prime_peer_js_02.js
+++ prime_peer_js_02.js
@@ -1,8 +1,43 @@
 
-//Code goes here
+
+
+
+function People(name, empNum, pay, debt){
+	this.name = name;
+	this.empNum = empNum;
+	this.pay = pay;
+	this.debt = debt;
+	
+
+	this.magic = function(){
+	return this.pay + this.debt / this.empNum + this.name;
+	};
+	this.log = function() {
+	this.log = console.log(this.magic());
+};
+
+}
+
+var array = [];
+
+for (i = 0; i <= 20; i++){
+var obj = new People(i, randomNumber(10, 50), randomNumber(1, 10), 54 )
+obj.log();
+array.push(obj);
+};
+
+
+
+
+
+
+
 
 //Utility function
 //A simple random number generator
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+
+
